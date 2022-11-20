a938956d92419598cad1daa639c32e0b09952066
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2022-11-20 15:16:16 +0000

    * Configs changes

--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -1,10 +1,10 @@
-import { interactions } from '../../interactions/main';
+import { interactions } from '@interactions/main';
 import { Command } from '../../structures/Command';
 import { client } from '../../'
 
 export default new Command({
 	interaction: interactions.ping,
 	excute: async ({ interaction }) => {
-		await interaction.reply({ content: `Pong! \`${client.ws?.ping}ms\`` });
+		await interaction?.reply({ content: `Pong! \`${client.ws?.ping}ms\`` });
 	},
 });
